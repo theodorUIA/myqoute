22ffcf8a2929407ce10a622009f275dd959efbb3
theodorUIA <89194128+theodorUIA@users.noreply.github.com>
2022-01-28 19:49:49 +0100

    added test

--- myqoute.go
+++ myqoute.go
@@ -8,28 +8,27 @@ import (
 	"rsc.io/quote"
 )
 
-func GetQuote() string {
+func RandNum(min int, max int) int {
 	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
+}
+
+func GetQuote() string {
 	min := 1
-	max := 2
-	var randNum = min + rand.Intn(max-min+1)
+	max := 4
+	var randNum = RandNum(min, max)
 	newquote := "Failed from start."
 	switch randNum {
 	case 1:
 		newquote = quote.Glass()
-		newquote = newquote
 	case 2:
 		newquote = quote.Go()
-		newquote = newquote
 	case 3:
 		newquote = quote.Opt()
-		newquote = newquote
 	case 4:
 		newquote = quote.Hello()
-		newquote = newquote
 	default:
 		newquote = "Failed to find number.."
-		newquote = newquote
 	}
 	return newquote
 

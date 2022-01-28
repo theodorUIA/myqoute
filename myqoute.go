505e14988c9af154e8693fc0deebc6392d103914
theodorUIA <89194128+theodorUIA@users.noreply.github.com>
2022-01-28 13:21:11 +0100

    added specified error msg

--- myqoute.go
+++ myqoute.go
@@ -11,22 +11,22 @@ import (
 func GetQuote() string {
 
 	var randNum = rand.Intn(4-1) + 1
-	newquote := "Failed from start"
+	newquote := "Failed from start." + " Num: " + strconv.Itoa(randNum)
 	switch randNum {
 	case 1:
-		newquote := quote.Glass()
+		newquote = quote.Glass()
 		newquote = newquote + ". Num: " + strconv.Itoa(randNum)
 	case 2:
-		newquote := quote.Go()
+		newquote = quote.Go()
 		newquote = newquote + ". Num: " + strconv.Itoa(randNum)
 	case 3:
-		newquote := quote.Opt()
+		newquote = quote.Opt()
 		newquote = newquote + ". Num: " + strconv.Itoa(randNum)
 	case 4:
-		newquote := quote.Hello()
+		newquote = quote.Hello()
 		newquote = newquote + ". Num: " + strconv.Itoa(randNum)
 	default:
-		newquote := "Failed to find number.."
+		newquote = "Failed to find number.."
 		newquote = newquote + ". Num: " + strconv.Itoa(randNum)
 	}
 	return newquote
